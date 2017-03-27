771c842a56e6ab39489be84d4e9021ab62187b46
Peter Elger <pelger@Peters-MacBook-Pro.local>
2017-03-27 21:57:27 +0100

    test

--- adderservice/build.sh
+++ adderservice/build.sh
@@ -11,15 +11,15 @@ case "$1" in
     npm test
   ;;
   container)
-    sudo -i -u pelger docker build -t adderservice:$GITSHA .
-    sudo -i -u pelger docker tag adderservice:$GITSHA pelger/adderservice:$GITSHA 
+    sudo -u pelger docker build -t adderservice:$GITSHA .
+    sudo -u pelger docker tag adderservice:$GITSHA pelger/adderservice:$GITSHA 
     sudo -i -u pelger docker push pelger/adderservice:$GITSHA 
   ;;
   deploy)
     sed -e s/_NAME_/adderservice/ -e s/_PORT_/8080/  < ../deployment/service-template.yml > svc.yml
     sed -e s/_NAME_/adderservice/ -e s/_PORT_/8080/ -e s/_IMAGE_/pelger\\/adderservice:$GITSHA/ < ../deployment/deployment-template.yml > dep.yml
-    sudo -i -u pelger kubectl apply -f svc.yml
-    sudo -i -u pelger kubectl apply -f dep.yml
+    sudo -i -u pelger kubectl apply -f $(pwd)/svc.yml
+    sudo -i -u pelger kubectl apply -f $(pwd)/dep.yml
   ;;
   *)
     echo 
