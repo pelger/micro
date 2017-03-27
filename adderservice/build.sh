437e5776e47c68bad826ace752eeb207587fb09c
Peter Elger <pelger@Peters-MacBook-Pro.local>
2017-03-27 13:40:31 +0100

    container build

---
+++ adderservice/build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+sh source ~/.bashrc
+
+GITSHA=$(git rev-parse --short HEAD)
+
+case "$1" in
+  build)
+    npm install
+  ;;
+  test)
+    npm test
+  ;;
+  container)
+    sudo docker build -t adderservice:$GITSHA .
+    sudo docker tag adderservice:$GITSHA pelger/adderservice:$GITSHA 
+    sudo docker push pelger/adderservice:$GITSHA 
+  ;;
+  deploy)
+    sed -e s/_NAME_/adderservice/ -e s/_PORT_/8080/  < ../deployment/service-template.yml > svc.yml
+    sed -e s/_NAME_/adderservice/ -e s/_PORT_/8080/ -e s/_IMAGE_/pelger\\/adderservice:$GITSHA/ < ../deployment/deployment-template.yml > dep.yml
+    sudo kubectl apply -f svc.yml
+    sudo kubectl apply -f dep.yml
+  ;;
+  *)
+    echo 
+    exit 1
+  ;;
+esac
+
